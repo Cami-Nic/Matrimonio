9c7289f0dbc26f5bcb524920fdc27e42febace3b
Cami-Nic <nic.signorelli97@gmail.com>
2026-09-11 23:38:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1513,7 +1513,15 @@ function startWeddingFireworks() {
    perché gennaio = 0.
 */
 
-const weddingDate = Date.now() + 10000;
+const weddingDate =
+    new Date(
+        2027,
+        3,
+        2,
+        0,
+        0,
+        1
+    ).getTime();
 
 /* =====================================================
    AGGIORNAMENTO COUNTDOWN
